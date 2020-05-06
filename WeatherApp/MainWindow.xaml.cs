09b617df879b8c9d2b314665a68cf76386f53585
Kobudzik <kobudzik98@gmail.com>
2020-05-06 09:22:06 +0200

    scratched some basic functionality- fetching xml data from web a,b printing only some of it

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WeatherApp
 {
@@ -23,6 +26,48 @@ namespace WeatherApp
         public MainWindow()
         {
             InitializeComponent();
+            string html = string.Empty;
+            string url = @"http://api.openweathermap.org/data/2.5/forecast?q=Tuchow&mode=xml&appid=2517431d46cd54e4f965409583890e1c&cnt=2&units=metric";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+
+
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Parse(html);
+
+            StringBuilder result = new StringBuilder();
+
+
+            var days =
+                from day
+                in xdoc.Descendants("time")
+                select new
+                {
+                    TemperatureValue = day.Element("temperature").Attribute("value").Value,
+                    TemperatureUnit = day.Element("temperature").Attribute("unit").Value
+                };
+            int curDay = 0;
+            foreach (var day in days)
+            {
+                result.AppendFormat("Day: {0} \n", curDay);
+                result.AppendLine(day.ToString());
+                curDay++;
+            }
+
+            myTextBox.Text = result.ToString();
+
+
+
         }
+
     }
 }
